e75dc2479081d6aac9fcbd0cbed2fd248d61b6f4
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-03-07 13:20:19 -0800

    played with css and fixed clock so it now works properly

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
 function realtimeClock() {
+
   let rtClock = new Date();
 
   let hours = rtClock.getHours();
   let minutes = rtClock.getMinutes();
   let seconds = rtClock.getSeconds();
 
-  //am/pm system
- let amPm = (hours < 12) ? 'AM' : 'PM';
-  // conver hours to 12-hr format
- hours = (hours > 12) ? hours - 12 : hours;
+  let amPm = (hours < 12) ? "AM" : "PM";
 
-//pad the hours, minutes and seconds with leading zeros
-hours = ('0' + hours).slice(-2);
-minutes = ('0' + hours).slice(-2);
-seconds = ('0' + hours).slice(-2);
+  hours = (hours > 12) ? hours - 12 : hours;
 
-//display the clock
-document.getElementById('clock').innerHTML =
-    hours + '  :  ' + minutes + '  :  ' + seconds + '  :  ' + amPm;
-    let t = setTimeout(realtimeClock, 500);
+  hours = ("0" + hours).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  seconds = ("0" + seconds).slice(-2);
+
+  document.getElementById('clock').innerHTML =
+    hours + "  :  " + minutes + "  :  " + seconds + " " + amPm;
+  let t = setTimeout(realtimeClock, 500);
+
 }
-
-
